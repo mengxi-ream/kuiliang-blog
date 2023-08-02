4fe3c0990f4b5077f0af24b6d4806ad209af8f4c
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-02 16:43:20 -0700

    finish chapter 6

---
+++ app/components/Listtern.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import getFormattedDate from "@/lib/getFormattedDate";
+
+type Props = {
+  post: BlogPost;
+};
+
+export function Listtern({ post }: Props) {
+  const { id, title, date } = post;
+  const formattedDate = getFormattedDate(date);
+  return (
+    <li className="mt-4 text-2xl dark:text-white/90" key={id}>
+      <Link
+        href={`/posts/${id}`}
+        className="underline hover:text-black/70 dark:hover:text-white"
+      >
+        {title}
+      </Link>
+      <br />
+      <p className="text-sm mt-1">{formattedDate}</p>
+    </li>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import { Posts } from "@/app/components/Posts";
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋 &nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold">Kuiliang</span>
+          I&apos;m <span className="font-bold">Kuiliang</span>
         </span>
       </p>
+      <Posts />
     </main>
   );
 }

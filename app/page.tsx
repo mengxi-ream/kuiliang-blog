fec4f9a65833feed136333afa351cd7321d4f5bf
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-03 15:36:07 -0700

    revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Posts } from "@/app/components/Posts";
 
+export const revalidate = 10;
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
---
+++ pages/api/revalidate.ts
@@ -0,0 +1,18 @@
+// http://localhost:1028/api/revalidate?path=/&secret=3995e8ff90282b1115619951ad37130b
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  const path = req.query.path as string;
+
+  await res.revalidate(path);
+
+  return res.json({ revalidated: true });
+}

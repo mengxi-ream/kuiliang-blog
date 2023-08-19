be38aab4ca467b9beb50ed7e52515d2921b17695
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-19 01:02:58 -0700

    add tool

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -15,6 +15,7 @@ export default function Header() {
   const pages = [
     { name: "Posts", href: "/" },
     { name: "Projects", href: "/" },
+    { name: "Tools", href: "/" },
   ];
 
   return (

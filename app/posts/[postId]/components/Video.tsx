417d40b0eca46366620abfd658a95330090149a9
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-19 11:17:31 -0700

    rounded lg iframe

--- app/posts/[postId]/components/Video.tsx
+++ app/posts/[postId]/components/Video.tsx
@@ -6,6 +6,7 @@ export default function Video({ id }: Props) {
   return (
     <div className="aspect-w-16 aspect-h-9">
       <iframe
+        className="rounded-lg"
         src={`https://www.youtube.com/embed/${id}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"

21b77677a344277cc031e348cbbc9f8d01893512
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-22 22:26:21 -0700

    fix style

--- app/components/ItemWithImage.tsx
+++ app/components/ItemWithImage.tsx
@@ -13,7 +13,9 @@ export default function ItemWithImage({
     <div className="my-10 flex flex-wrap gap-x-2">
       <div
         className={
-          imageSrc ? "flex-auto w-full md:w-64 p-4 max-w-md mx-auto" : "hidden"
+          imageSrc
+            ? "flex-auto w-full md:w-64 p-4 max-w-md md:max-w-sm mx-auto"
+            : "hidden"
         }
       >
         <div className="relative w-full aspect-w-16 aspect-h-9">
@@ -31,7 +33,9 @@ export default function ItemWithImage({
           )}
         </div>
       </div>
-      <div className="flex-auto w-full md:w-72">{children}</div>
+      <div className="flex-auto w-full md:w-72 max-w-md md:max-w-lg mx-auto">
+        {children}
+      </div>
     </div>
   );
 }

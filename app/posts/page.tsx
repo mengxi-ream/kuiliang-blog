56cd2bdd402d33fc6c4276588d7dadc839d4083b
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-24 01:09:06 -0700

    return posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -14,5 +14,21 @@ export default async function Posts() {
     uniqueTagsArray = Array.from(uniqueTags); // Converts the Set back to an array, if you need it as an array
   }
 
-  return <PostLoader uniqueTagsArray={uniqueTagsArray} />;
+  return (
+    <div className="md:flex gap-x-4">
+      <div className="w-full md:flex-auto">
+        <PostList />
+      </div>
+      <section className="w-full md:flex-auto md:w-64 mt-14">
+        <h2 className="text-2xl font-bold mb-4">Tags</h2>
+        {uniqueTagsArray.map((tag) => (
+          <Link href={`tags/${tag}`} key={tag} className="block my-1 ml-2">
+            <span className="mr-2 text-gray-500 text-sm dark:text-gray-400">
+              {tag}
+            </span>
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
 }

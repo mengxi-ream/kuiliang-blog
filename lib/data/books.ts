fc78c6e74d04cfe0b890f40f5b0c93785d25d813
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-08-29 16:04:07 -0700

    add a book

--- lib/data/books.ts
+++ lib/data/books.ts
@@ -11,7 +11,7 @@ const books: Array<Product> = [
   {
     name: "The Book of Why",
     imageSrc: "/images/books/the-book-of-why.jpg",
-    tags: ["Judea Pearl", "Dana Mackenzie", "Data Science", "Causality"],
+    tags: ["Judea Pearl", "Dana Mackenzie", "Causality"],
     description:
       "The New Science of Cause and Effect. It is a popular science book about the use of causal inference.",
     time: "Mar 2022",
@@ -26,6 +26,15 @@ const books: Array<Product> = [
     time: "Sep 2023",
     mainLink:
       "https://www.packtpub.com/product/python-object-oriented-programming-fourth-edition/9781801077262",
+  },
+  {
+    name: "The Functional Art",
+    imageSrc: "/images/books/the-functional-art.jpg",
+    tags: ["Alberto Cairo", "Data Visualization"],
+    description:
+      "An Introduction to Information Graphics and Visualization. It is a book about data visualization.",
+    time: "Feb 2019",
+    mainLink: "http://www.thefunctionalart.com",
   },
 ];
 

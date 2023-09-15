be037d892d64ff501e62f349e1a4410ee1fd8a31
Kuiliang Zhang <kzhang@student.ubc.ca>
2023-09-15 12:10:22 -0700

    no tag

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -25,21 +25,6 @@ export default async function Posts({
       <div className="w-full md:flex-auto">
         <PostList tags={tags} />
       </div>
-      <section className="w-full md:flex-auto md:w-64 mt-14">
-        <h2 className="text-2xl font-bold mb-4">Tags</h2>
-        <div className="flex flex-wrap mb-10 gap-x-1.5 gap-y-1">
-          {uniqueTagsArray.map((tag) => (
-            <Link href={processURL(tags, tag)} key={tag}>
-              <Tag
-                key={tag}
-                content={tag}
-                size="sm"
-                className={processLinkStyle(tags, tag)}
-              />
-            </Link>
-          ))}
-        </div>
-      </section>
     </div>
   );
 }
@@ -52,28 +37,4 @@ function processTags(
   }
 
   return tags;
-}
-
-function processURL(tags: string[] | undefined, tag: string): string {
-  if (!tags) {
-    return `/posts?tag=${tag}`;
-  }
-
-  let tagQuery;
-
-  if (!tags.includes(tag)) {
-    tagQuery = `tag=${tag}&` + tags.map((t) => `tag=${t}`).join("&");
-  } else {
-    // remove tag from tags then generate tagQuery
-    const newTags = tags.filter((t) => t !== tag);
-    tagQuery = newTags.map((t) => `tag=${t}`).join("&");
-  }
-
-  return `/posts?${tagQuery}`;
-}
-
-function processLinkStyle(tags: string[] | undefined, tag: string): string {
-  return tags && tags.includes(tag)
-    ? "rounded-md ring-1 ring-black dark:ring-white"
-    : "";
 }
